b16c40c1cd7ab938fe73a4c821c8e5b8ccd74321
Narender <Narender@IST-DEV>
2017-02-13 17:17:56 +0530

    Home changes

--- VoyagerApp/www/Contents/scripts/HomeProductCtrl.js
+++ VoyagerApp/www/Contents/scripts/HomeProductCtrl.js
@@ -1,14 +1,19 @@
 ﻿scotchApp.controller('mainController', function ($scope, $http, $routeParams) {
     $scope.$watch('search', function () {
         Deatils();
+        $rootScope.$broadcast('status_updated', { status: 10 });
+        $rootScope.$on('status_updated', function (event, obj) {
+            console.log(obj.status); //10
+        })
     });
+
     function Deatils() {
         var data = $http({
             method: 'GET',
             url: 'http://staging.sirez.com/babyhugzservice/ProductApi.svc/GetHomeProductList',
         }).then(function successCallback(response) {            
             $scope.GetHomeProductList = response.data.ResultData;
-            
+
         }, function errorCallback(response) {
             alert('Ohh Some error');
         });
